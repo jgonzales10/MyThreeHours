65b7931a20198cdf8f53eca92a47ba06b0d48b35
jgonzales10 <jgonzales10@miners.utep.edu>
2018-08-04 02:17:33 -0600

    Connected MainActivity and AddCourseActivity with button. TODO: Hard part, implement ListView for classes with a progress bar & (+/-) time studied.

--- app/src/main/java/edu/utep/cs/cs4330/mythreehours/MainActivity.java
+++ app/src/main/java/edu/utep/cs/cs4330/mythreehours/MainActivity.java
@@ -1,13 +1,23 @@
 package edu.utep.cs.cs4330.mythreehours;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button addClassButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void addClassClicked(View view) {
+        Intent intent = new Intent(MainActivity.this, AddCourseActivity.class);
+        startActivity(intent);
     }
 }
